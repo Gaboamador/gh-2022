820f406b8865fb967b9988d57c2e08540845edb9
Gaboamador <gaboamador@hotmail.com>
2024-03-05 13:47:14 -0300

    imagenes nuevos participantes

--- src/data/participantsToImage.js
+++ src/data/participantsToImage.js
@@ -4,10 +4,13 @@ import Axel from '../pictures/Axel.png'
 import Bautista from '../pictures/Bautista.png'
 import Carla from '../pictures/Carla.png'
 import Catalina from '../pictures/Catalina.png'
+import Damián from '../pictures/Damian.png'
+import Darío from '../pictures/Dario.png'
 import Denisse from '../pictures/Denisse.png'
 import Emmanuel from '../pictures/Emmanuel.png'
 import Federico from '../pictures/Federico.png'
 import Florencia from '../pictures/Florencia.png'
+import FlorenciaC from '../pictures/FlorenciaC.png'
 import Hernán from '../pictures/Hernan.png'
 import Isabel from '../pictures/Isabel.png'
 import Joel from '../pictures/Joel.png'
@@ -15,7 +18,9 @@ import Juliana from '../pictures/Juliana.png'
 import Lisandro from '../pictures/Lisandro.png'
 import Lucía from '../pictures/Lucia.png'
 import Martín from '../pictures/Martin.png'
+import Mauro from '../pictures/Mauro.png'
 import Nicolás from '../pictures/Nicolas.png'
+import Paloma from '../pictures/Paloma.png'
 import Rosina from '../pictures/Rosina.png'
 import Sabrina from '../pictures/Sabrina.png'
 import Virginia from '../pictures/Virginia.png'
@@ -29,10 +34,13 @@ export const participantsToImage = {
     Bautista: Bautista,
     Carla: Carla,
     Catalina: Catalina,
+    Damián: Damián,
+    Darío: Darío,
     Denisse: Denisse,
     Emmanuel: Emmanuel,
     Federico: Federico,
     Florencia: Florencia,
+    FlorenciaC: FlorenciaC,
     Hernán: Hernán,
     Isabel: Isabel,
     Joel: Joel,
@@ -40,7 +48,9 @@ export const participantsToImage = {
     Lisandro: Lisandro,
     Lucía: Lucía,
     Martín: Martín,
+    Mauro: Mauro,
     Nicolás: Nicolás,
+    Paloma: Paloma,
     Rosina: Rosina,
     Sabrina: Sabrina,
     Virginia: Virginia,
